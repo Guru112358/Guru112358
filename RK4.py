d648afd0e7e46dff00c448bf8a6fd35cf9fab4d8
Guruprasad S <61557979+Guru112358@users.noreply.github.com>
2020-07-06 21:54:28 +0530

    Update RK4.py

--- RK4.py
+++ RK4.py
@@ -1,31 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 12 23:20:28 2020
+Created on Tue Jun  9 22:32:05 2020
 
 @author: Guru
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math 
-#defining function on RHS
+
+
+#simple program to solve ODE by Runge Kutta 4th order method
+#creating a function to do the numerical tasks
+def RK4(x0,y0,xp,h):
+    x=[]
+    y=[]
+    n=(xp-x0)/h
+    for i in range(int(n)):
+        k1=h*f(x0,y0)
+        x2=x0+h/2
+        y2=y0+k1/2
+        k2=h*f(x2,y2)
+        x3=x0+h/2
+        y3=y0+(k2/2)
+        k3=h*f(x3,y3)
+        x4=x0+h
+        y4=y0+k3
+        k4=h*f(x4,y4)
+        x0=(x0+h)
+        y0=(y0+(1.0/6.0)*(k1+2*k2+2*k3+k4))
+        x.append(x0)
+        y.append(y0)
+        print(x,y)
+        plt.plot(x,y)
+
 def f(x,y):
-    f=x+y
+    f=(5*x**2-y)/(np.exp(x+y))
     return f
 
-x=input("enter initial value of x: ")
-y=input("enter initial value of y: ")
-h=input("enter the step: ")
-xp=input("enter value of x at which yis required:")
-n=int((xp-x)/h)
-for i in range(n):   
-    k1=h*f(x,y)
-    k2=h*f((x+(h/2)),(y+(k1/2)))
-    k3=h*f((x+(h/2)),(y+(k2/2)))
-    k4=h*f((x+h),(y+k3))
-    x=x+h
-    y=y+((1.0/6.0)*(k1+(2*k2)+(2*k3)+k4)
-    print(x,y)
 
-
-
+print(RK4(0,1,10,0.1))
